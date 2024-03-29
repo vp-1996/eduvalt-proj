8884e61335156e9e1c6c716acf6c802fbe825814
Vaibhav Parashar <parasharv592@gmail.com>
2024-03-29 18:20:20 +0530

    let commit

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -8,15 +8,15 @@ function BasicExample() {
         <Navbar style={{ marginBottom: "125%", }} bg='dark' expand="lg" className="bg-body-tertiary">
             <Container style={{ background: "white" }}>
                 <Navbar.Brand href="/">
-                    <img style={{ width:"150px"}} src='src/assets/logo.png' />
+                    <img style={{ width: "150px", marginLeft: "100px" }} src='src/assets/logo.png' />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link id='NavLink' className='navs' href="/">Home</Nav.Link>
-                        <Nav.Link className='navs' href="/AllCourses">Courses</Nav.Link>
-                        <Nav.Link className='navs' href="/AboutUs">About Us</Nav.Link>
-                        <Nav.Link className='navs' href="/Contact">Contact</Nav.Link>
+                        <Nav.Link id='NavLink' className='navs' href="/AllCourses">Courses</Nav.Link>
+                        <Nav.Link id='NavLink' className='navs' href="/AboutUs">About Us</Nav.Link>
+                        <Nav.Link id='NavLink' className='navs' href="/Contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,9 +3,30 @@ import BasicExample from '../components/NavBar'
 
 const Home = () => {
   return (
-    
+
     <>
-    <BasicExample/>
+      <div className='section1'>
+        <BasicExample />
+
+        <div>
+          <div className='part1'>
+            <h6
+              style={{ color: "#1363DF", background: "#E7EFFC", width: "25%", marginTop: "20%" }}
+            >
+              100% Satisfaction Guarantee
+            </h6>
+
+
+          </div>
+
+          <div className='part2'>
+
+          </div>
+
+        </div>
+
+      </div>
+
     </>
   )
 }
