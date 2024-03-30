9db9f8bef00b730a7be8f2ebb1fac5b9af8d437b
Vaibhav Parashar <parasharv592@gmail.com>
2024-03-30 09:51:47 +0530

    new commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,25 +5,29 @@ const Home = () => {
   return (
 
     <>
-      <div className='section1'>
-        <BasicExample />
 
-        <div>
-          <div className='part1'>
-            <h6
-              style={{ color: "#1363DF", background: "#E7EFFC", width: "25%", marginTop: "20%" }}
-            >
-              100% Satisfaction Guarantee
-            </h6>
+      <BasicExample />
 
+      <div style={{ display: "flex", marginTop: "-1100px", width: "1330px" }} className='mainDiv'>
 
-          </div>
+        <div className='part1' style={{ width: "200px" }}>
+          <h5
+            style={{ color: "#1363DF", background: "#E7EFFC", marginTop: "20%", width: "190%", marginLeft: "30%" }}
+          >
+            100% Satisfaction...
+          </h5>
 
-          <div className='part2'>
+          <p style={{ fontSize: "45px" }}>
+            Learn <span>Skills</span> From <br />
+            Our Top Instructors
+          </p>
 
-          </div>
 
         </div>
+
+        {/* <div style={{ paddingLeft: "50%" }} className='part2'> */}
+        <img style={{ width: "502px", height: "529px", marginLeft: "600px" }} src='/src/assets/banner_img.png' />
+        {/* </div> */}
 
       </div>
 
